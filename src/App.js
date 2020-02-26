a00260eb7a73816264e2e670ff9b1949981d9c0e
Austin White <AustinWhite93@gmail.com>
2020-02-25 23:13:21 -0800

    create react app boilerplate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <sorting_visualizer></sorting_visualizer>
+    </div>
+  );
+}
+
+export default App;
